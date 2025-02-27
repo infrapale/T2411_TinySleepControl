6c9227e341c7e14582597edf745517e80d205779
Tom Hoglund <infrapale@gmail.com>
2025-02-27 13:21:09 +0200

    Update T2411_TinySleepControl_3.ino
    Fixing some comments

--- T2411_TinySleepControl_3/T2411_TinySleepControl_3.ino
+++ T2411_TinySleepControl_3/T2411_TinySleepControl_3.ino
@@ -3,45 +3,14 @@
 T2411 ATtiny412 Sleep Control
 - I2C slave interface
 - Cut main MCU and peripheral power on timeout or direct command
-- EEPROM functions
-- Low power to be implemented
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-I was able to get down to 2uA in sleep mode but when connecting the trigger 
-input to GND the current incresed to 1mA in idle mode.  This also applied for the 
-Sleeping-Lighthouse example code
-==> F A I L E D
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 -------------------------------------------------------------------------------
 https://github.com/infrapale/T2411_TinySleepControl.git
 Sleep function based on the : 
-  https://github.com/microPaul/ATtiny412-Sleeping-Lighthouse
-
+https://github.com/microPaul/ATtiny412-Sleeping-Lighthouse
 https://github.com/SpenceKonde/megaTinyCore#readme
 https://wiki-content.arduino.cc/en/Tutorial/LibraryExamples/MasterWriter
 https://inductive-kickback.com/2019/04/creating-an-i2c-slave-interface-for-a-sensor-or-peripheral/
-
-See python scripts for usage
-
-High Voltage setting;
-  C:\Users\tom_h\AppData\Local\Arduino15\packages\megaTinyCore\hardware\megaavr\2.6.10\boards.txt
-    Uncomment 4 last rows
-    #----------------------------------------#
-    # UPDI/RESET PIN CONFIGURATION           #
-    #________________________________________#
-    atxy7.menu.resetpin.UPDI=UPDI (no reset pin)
-    atxy7.menu.resetpin.UPDI.bootloader.resetpinbits=01
-    atxy7.menu.resetpin.UPDIaltrst=UPDI w/reset on PB4 (2-series only)
-    atxy7.menu.resetpin.UPDIaltrst.bootloader.resetpinbits={bootloader.altreset}
-    #Expert mode - if you understand what these mean, uncomment them to enable.
-    #there are a few lines like this in platform.txt too that can be uncommented to enable for uploads, not just when burning bootloader
-    atxy7.menu.resetpin.reset=Reset (DANGER - Bricks chip w/out HV UPDI programmer!)
-    atxy7.menu.resetpin.reset.bootloader.resetpinbits=10
-    atxy7.menu.resetpin.gpio=GPIO (DANGER - Bricks chip w/out HV UPDI programmer - Use PCHV)
-    atxy7.menu.resetpin.gpio.bootloader.resetpinbits=00
-
-
 
 *******************************************************************************/
 
